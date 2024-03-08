cdbb121bc808a0dd97b4d8bd1b50700e91d5612d
Heet Solanki <heetsolanki2006@gmail.com>
2024-03-08 17:36:11 +0530

    Create Fibonacci.js

--- 8th March/Factorial/Factorial.js
+++
@@ -1,7 +0,0 @@
-let number = parseInt(prompt("Enter a number: "));
-let fact = 1;
-for(let i = 1; i <= number; i++)
-{
-    fact = fact * i;
-}
-document.getElementById("factorial").innerHTML="Factorial of " + number + " is " + fact;
