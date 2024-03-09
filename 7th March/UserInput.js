a0a98b7045c9b31238227c5a23faaefbdcf65064
Heet Solanki <heetsolanki2006@gmail.com>
2024-03-09 12:57:48 +0530

    Create UserInput.js

---
+++ 7th March/UserInput.js
@@ -0,0 +1,5 @@
+let numOne = parseInt(prompt("Enter value for numOne: "))
+let numTwo = parseInt(prompt("Enter value for numTwo: "))
+let numThree = parseInt(prompt("Enter value for numThree: "))
+let sum = numOne + numTwo + numThree;
+document.getElementById("sum").innerHTML="Sum of " + numOne + " + " + numTwo + " + " + numThree + " = " + sum;
